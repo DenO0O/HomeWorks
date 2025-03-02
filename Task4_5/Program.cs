df181db400547175e02d52919903c5a8c4318fa2
Денис <deny001@yandex.ru>
2025-03-02 20:15:30 +0500

    Задание 4

---
+++ Task4_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запрашивать у пользователя число до тех пор, пока он не введет число из диапазона [20; 60]
+            int a;
+            do
+            {
+                Console.Write("Число от 20 до 60: ");
+                a = Convert.ToInt32(Console.ReadLine());
+            } while (a<=20 || a>=60);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task4_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task4_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводится n. Определить, является ли оно степенью 2 - ки ?
+            Console.Write("Число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int r = 0; //Счтает от степени 0
+
+            while (Math.Pow(2, r) < n)
+            {
+                r++;
+            }
+
+            if (Math.Pow(2, r) == n)
+                Console.WriteLine($"{n} это степень 2");
+            else
+                Console.WriteLine($"{n} это не степень 2");
+
+            Console.ReadKey();
+        }
+    }
+}
