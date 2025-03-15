ef651eeb5ac3ad59334b8c609beaa69fcf8ea9d4
Денис <deny001@yandex.ru>
2025-03-16 00:25:27 +0500

    Задание 6

---
+++ Task6_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Составить регулярное выражение для проверки корректности номера телефона.
+            //Корректный номер имеет формат +7(123)456-78-90
+
+            string[] testInputs = {
+                "+7(123)456-78-90",
+                "+9(321)556-79-91",
+                "+9(321)55678-90",
+                "+9(321)556-7890",
+                "+9321)556-78-90",
+                "+7(123)4f6-78-90",
+                "+f(123)456-78-90"
+            };
+
+            Regex regex = new Regex(@"^\+[0-9]\([0-9]{3}\)[0-9]{3}\-[0-9]{2}\-[0-9]{2}$");
+
+            foreach (var item in testInputs)
+            {
+                if (regex.IsMatch(item))
+                    Console.WriteLine($"{item} - ok");
+                else
+                    Console.WriteLine($"{item} - X");
+            }
+            Console.ReadKey();
+        }
+    }
+}
