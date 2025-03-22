3bacb2b985c5004571aaebd9e706202f92ba7487
Денис <deny001@yandex.ru>
2025-03-22 20:43:32 +0500

    Задание 7

---
+++ Task7_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Два треугольника заданы длинами своих сторон. Определите, площадь какого из них больше
+            //(создайте метод Square для вычисления площади треугольника по длинам его сторон).  Для решения задачи можно
+            //использовать формулу Герона.
+
+            double[] figure1 = new double[3];
+            double[] figure2 = new double[3];
+            Console.Write("Треугольник 1. Сторона А: ");
+            figure1[0] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Треугольник 1. Сторона B: ");
+            figure1[1] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Треугольник 1. Сторона C: ");
+            figure1[2] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Треугольник 2. Сторона А: ");
+            figure2[0] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Треугольник 2. Сторона B: ");
+            figure2[1] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Треугольник 2. Сторона C: ");
+            figure2[2] = Convert.ToDouble(Console.ReadLine());
+
+            double figure1Square = Square(figure1);
+            double figure2Square = Square(figure2);
+            string result = "1 равна площади треугольника 2";
+
+            if (figure1Square > figure2Square) result = "1 больше";
+            else if (figure1Square < figure2Square) result = "2 больше";
+
+            Console.WriteLine($"Площадь треугольника {result}");
+            Console.ReadKey();
+        }
+
+        static double Square(double[] figure)
+        {
+            //Вычисление площади по формуле Герона
+            double p = (figure[0] + figure[1] + figure[2]) / 2;
+            return Math.Sqrt(p * (p - figure[0]) * (p - figure[1]) * (p - figure[2]));
+        }
+    }
+}
---
+++ Task7_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создайте метод CalcCube для вычисления объема и площади поверхности куба по длине его ребра. Метод должен иметь 3
+            //параметра - длина ребра, out-параметр для объема и out-параметр для площади поверхности.
+
+            double edgeLength = 5;
+            double volume, surfaceArea;
+            CalcCube(edgeLength, out volume, out surfaceArea);
+            Console.WriteLine($"Объем куба: {volume}");
+            Console.WriteLine($"Площадь поверхности куба: {surfaceArea}");
+            Console.ReadKey();
+        }
+
+        static void CalcCube(double edgeLength, out double volume, out double surfaceArea)
+        {
+            if (edgeLength > 0)
+            {
+                volume = Math.Pow(edgeLength, 3);
+                surfaceArea = edgeLength * edgeLength * 6;
+            }
+            else
+            {
+                volume = 0;
+                surfaceArea = 0;
+            }
+        }
+    }
+}
