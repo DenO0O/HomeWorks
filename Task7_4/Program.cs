659e8b7389989ab1d7435fcb04515641cd14271d
Денис <deny001@yandex.ru>
2025-03-22 20:43:32 +0500

    Задание 7

---
+++ Task7_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишите метод FindMax, который принимает переменное количество чисел и возвращает максимальное значение.
+            //Используйте ключевое слово params
+
+            int maxNumber = FindMax(10, 25, 5, 30, 15);
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.ReadKey();
+        }
+
+        private static int FindMax(params int[] v1)
+        {
+            int maxNumber = 0;
+            if (v1.Length > 0)
+            {
+                for (int i = 0; i < v1.Length; i++)
+                {
+                    if (v1[i] > maxNumber) maxNumber = v1[i];
+                }
+            }
+            return maxNumber;
+        }
+    }
+}
---
+++ Task7_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task7_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишите перегруженные методы Multiply, которые могут умножать два числа (целые или дробные) и возвращать результат
+            Console.WriteLine(Multiply(2, 3));
+            Console.WriteLine(Multiply(2.5, 3.5));
+            Console.ReadKey();
+        }
+
+        private static int Multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+
+        private static double Multiply(double v1, double v2)
+        {
+            return v1 * v2;
+        }
+    }
+}
