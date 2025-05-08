b411ac514fce89af97543742957d69c0a24224f0
Денис <deny001@yandex.ru>
2025-05-08 19:09:03 +0500

    Задание 13

---
+++ Task13_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task13_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = new Random().Next(1, 101);
+            int m = 0;
+            int userinput = -1;
+            bool isBreaked = false;
+
+            Console.WriteLine("Угадайте число от 1 до 100 (0 - закончить):");
+            while (secretNumber != userinput)
+            {
+                try
+                {
+                    Console.Write("> ");
+                    userinput = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(" Введено не число. Попробуйте еще раз");
+                    continue;
+                }
+                if (userinput == 0)
+                {
+                    isBreaked = true;
+                    break;
+                }
+                m++;
+                if (secretNumber > userinput)
+                    Console.WriteLine("Больше");
+                else if (secretNumber < userinput)
+                    Console.WriteLine("Меньше");
+            }
+
+            if (isBreaked)
+                Console.WriteLine($"Остановлено. Было загадано число {secretNumber}");
+            else
+                Console.WriteLine($"Число {secretNumber} угадано. Кол-во попыток: {m}");
+            Console.ReadKey();
+        }
+    }
+}
