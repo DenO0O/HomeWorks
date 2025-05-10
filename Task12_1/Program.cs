21390cc5d58d2381cb8ea73be0922a49f400491e
Денис <deny001@yandex.ru>
2025-05-10 16:05:37 +0500

    изменено по замечанию

--- Task12_1/Program.cs
+++ Task12_1/Program.cs
@@ -20,45 +20,45 @@ namespace Task12_1
                     new Book<int, string>(42, "Книга 3", "1900", "И.О. Автор3"),
                 };
 
-            Console.WriteLine(Book<string, int>.FindBook(books1, "F-1234"));
-            Console.WriteLine(Book<int, string>.FindBook(books2, 42));
+            Console.WriteLine(FindBook<string, int>(books1, "F-1234"));
+            Console.WriteLine(FindBook<int, string>(books2, 42));
             Console.ReadKey();
         }
 
-        class Book<T, U>
+        public static Book<T, U> FindBook<T, U>(Book<T, U>[] books, T code)
         {
-            public T Code { get; set; }
-
-            public string Title { get; set; }
-
-            public U PublicationYear { get; set; }
-
-            public string Author { get; set; }
-
-            public Book(T code, string title, U publicationYear, string author)
+            foreach (var book in books)
             {
-                Code = code;
-                Title = title;
-                PublicationYear = publicationYear;
-                Author = author;
-            }
-
-            public override string ToString()
-            {
-                return $"Код: {Code} ({typeof(T).Name}), Название: {Title}, Автор: {Author}, Год: {PublicationYear} ({typeof(U).Name})";
-            }
-
-            public static Book<T, U> FindBook(Book<T, U>[] books, T code)
-            {
-                foreach (var book in books)
+                if (book.Code.Equals(code))
                 {
-                    if (book.Code.Equals(code))
-                    {
-                        return book;
-                    }
+                    return book;
                 }
-                return null;
             }
+            return null;
+        }
+    }
+
+    class Book<T, U>
+    {
+        public T Code { get; set; }
+
+        public string Title { get; set; }
+
+        public U PublicationYear { get; set; }
+
+        public string Author { get; set; }
+
+        public Book(T code, string title, U publicationYear, string author)
+        {
+            Code = code;
+            Title = title;
+            PublicationYear = publicationYear;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"Код: {Code} ({typeof(T).Name}), Название: {Title}, Автор: {Author}, Год: {PublicationYear} ({typeof(U).Name})";
         }
     }
 }
