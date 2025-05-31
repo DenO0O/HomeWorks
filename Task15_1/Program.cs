df3a98f80dfe6bac5297a0c0f77fff576f3989b7
Денис <deny001@yandex.ru>
2025-05-31 18:41:31 +0500

    Задание 15

---
+++ Task15_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task15_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var gamers = new List<string>();
+            gamers.Add("Иванов");
+            gamers.Add("Петров");
+            gamers.Add("Сидоров");
+
+            //Вставка игрока "Козлов" на позицию с индексом 1 методом Insert.
+            gamers.Insert(1, "Козлов");
+
+            //Проверка наличия игрока "Петров" в команде методом Contains.
+            Console.WriteLine($"Есть ли игрок 'Петров' в команде? {gamers.Contains("Петров")}");
+
+            //Удаление игрока "Сидоров" методом Remove.
+            bool resultRemove = gamers.Remove("Сидоров");
+
+            //Поиск индекса игрока "Козлов" методом IndexOf.
+            Console.WriteLine($"Индекс игрока 'Козлов': {gamers.IndexOf("Козлов")}");
+
+            //Сортировка списка по алфавиту методом Sort.
+            gamers.Sort();
+
+            // Выведите текущий состав команды
+            Console.WriteLine($"Текущий состав команды:");
+            for (int i = 0; i < gamers.Count; i++)
+            {
+                Console.WriteLine($" {i + 1}. {gamers[i]}");
+            }
+
+            //Проверьте, пуст ли список (свойство Count), и очистите его методом Clear
+            Console.WriteLine($"Команда пуста? {gamers.Count == 0}");
+            gamers.Clear();
+            Console.WriteLine($"Количество игроков после очистки: {gamers.Count}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task15_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task15_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> goods = new Dictionary<string, int>();
+
+            goods.Add("A001", 10);
+            goods.Add("B205", 25);
+            goods.Add("C307", 15);
+
+            //Проверьте наличие товара с артикулом "B205" (ContainsKey).
+            Console.WriteLine($"Есть ли товар с артикулом B205? {goods.ContainsKey("B205")}");
+
+            //Обновите количество ноутбуков до 8(продали 2 шт.) через индексатор.
+            goods["A001"] = 8;
+
+            //Получите количество наушников безопасным способом (TryGetValue).
+            if (goods.TryGetValue("C307", out int value))
+                Console.WriteLine($"Количество наушников: {value}");
+
+            //Увеличьте количество смартфонов на 5 (новый завоз).
+            goods["B205"] += 5;
+
+            //Удалите наушники из инвентаря (Remove).
+            goods.Remove("C307");
+
+            //Выведите текущий инвентарь
+            Console.WriteLine($"Текущий инвентарь:");
+            foreach (var g in goods)
+            {
+                Console.WriteLine($"Артикул: {g.Key}, Количество: {g.Value}");
+            }
+
+            //Проверьте, пуст ли словарь (Count), затем полностью очистите инвентарь (Clear)
+            Console.WriteLine($"Инвентарь пуст? {goods.Count == 0}");
+            goods.Clear();
+            Console.WriteLine($"Количество товаров после очистки: {goods.Count}");
+            Console.ReadKey();
+        }
+    }
+}
