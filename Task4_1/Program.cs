2d42fa18fa50e52670426ed561c92c1b9e18b676
Денис <deny001@yandex.ru>
2025-06-21 23:53:55 +0500

    1

---
+++ Task4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводится натуральное число n. Найти n! Например, 6! = 1 * 2 * 3 * 4 * 5 * 6
+            Console.Write("Число: ");
+            int digit = Convert.ToInt32(Console.ReadLine());
+
+            double result = 1;
+            for (int i = 2; i <= digit; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine($"{digit}!={result}");
+            Console.ReadKey();
+        }
+    }
+}
